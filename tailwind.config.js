42996e92d532155274a130e2b96e2e1e072377bd
Sanjay Soundarajan <sanjay.16@live.com>
2022-10-17 16:21:00 -0700

    Add button mdx

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 // eslint-disable-next-line no-undef
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,mdx,stories.js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx,mdx,stories.js,stories.mdx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
